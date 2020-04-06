cd24db4ec362ccd6c006d9d32c626b510a7be36c
prithviraj-maurya <pmaurya196@gmail.com>
2020-04-06 13:13:31 +0530

    Updates

---
+++ precache-manifest.cb2c509d11b140278945c237981b6d40.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc3514e8193ebaa6f22741b025a49e1",
+    "url": "/expenses-d3-react/index.html"
+  },
+  {
+    "revision": "7b20ad13bc13ebd82f48",
+    "url": "/expenses-d3-react/static/css/main.864f9a14.chunk.css"
+  },
+  {
+    "revision": "34c44ae25dbfb826abc1",
+    "url": "/expenses-d3-react/static/js/2.fcbe3e45.chunk.js"
+  },
+  {
+    "revision": "2aa452a43c496865ee4e2291133adeed",
+    "url": "/expenses-d3-react/static/js/2.fcbe3e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b20ad13bc13ebd82f48",
+    "url": "/expenses-d3-react/static/js/main.1989be61.chunk.js"
+  },
+  {
+    "revision": "047cba1e596d3ceee1d3",
+    "url": "/expenses-d3-react/static/js/runtime-main.67df2d2f.js"
+  },
+  {
+    "revision": "947af042f328b2f7732fcae021170599",
+    "url": "/expenses-d3-react/static/media/CatMule-Caps.947af042.otf"
+  },
+  {
+    "revision": "e6d1a273c8000ee917d4b27c24282e6a",
+    "url": "/expenses-d3-react/static/media/LibreBaskerville-Bold.e6d1a273.ttf"
+  },
+  {
+    "revision": "d32e21a107beb9e1c5cfa098733433ce",
+    "url": "/expenses-d3-react/static/media/LibreBaskerville-Italic.d32e21a1.ttf"
+  },
+  {
+    "revision": "27b9efe7ae34478c82311efb90011607",
+    "url": "/expenses-d3-react/static/media/LibreBaskerville-Regular.27b9efe7.ttf"
+  },
+  {
+    "revision": "b6937761491a737f6e556ff1109407e0",
+    "url": "/expenses-d3-react/static/media/delete.b6937761.svg"
+  }
+]);
